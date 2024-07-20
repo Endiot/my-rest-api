b9afd662f3a4d3ed85b048838a98155ce1bba343
Endiot <endienstein@gmail.com>
2024-07-20 20:54:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'Zeeoneofc'// endyot
 global.MONGO_DB_URI = "" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "" //endienstein@gmail.com
+global.email_password = "" //kkkn sals halm bbbt
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
